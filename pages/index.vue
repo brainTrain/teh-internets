57f147cd44d0bf67484ea3cf2e90e6b144654f35
brainTrain <abductee@gmail.com>
2018-09-19 22:23:10 -0700

    hello internets

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container">
+    <h1 class="title">A/S/L?</h1>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-size: 10px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
+
